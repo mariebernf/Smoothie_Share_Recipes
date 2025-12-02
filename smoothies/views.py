ef68d76e8b57bd1377f9fa54815273bf910ea88f
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-02 09:48:27 +0100

    Create smoothies views

--- smoothies/views.py
+++ smoothies/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Smoothie
+from .forms import SmoothieForm
 
-# Create your views here.
+def smoothie_list(request):
+    smoothies = Smoothie.objects.all().order_by('-date_created')
+    return render(request, 'smoothies/smoothie_list.html', {'smoothies': smoothies})
+
+@login_required
+def smoothie_create(request):
+    if request.method == 'POST':
+        form = SmoothieForm(request.POST)
+        if form.is_valid():
+            smoothie = form.save(commit=False)
+            smoothie.author = request.user
+            smoothie.save()
+            return redirect('smoothie_list')
+    else:
+        form = SmoothieForm()
+    return render(request, 'smoothies/smoothie_form.html', {'form': form})
+
+@login_required
+def smoothie_update(request, pk):
+    smoothie = get_object_or_404(Smoothie, pk=pk)
+
+    if smoothie.author != request.user:
+        return redirect('smoothie_list')
+
+    if request.method == 'POST':
+        form = SmoothieForm(request.POST, instance=smoothie)
+        if form.is_valid():
+            form.save()
+            return redirect('smoothie_list')
+    else:
+        form = SmoothieForm(instance=smoothie)
+
+    return render(request, 'smoothies/smoothie_form.html', {'form': form})
+
+@login_required
+def smoothie_delete(request, pk):
+    smoothie = get_object_or_404(Smoothie, pk=pk)
+
+    if smoothie.author != request.user:
+        return redirect('smoothie_list')
+
+    if request.method == 'POST':
+        smoothie.delete()
+        return redirect('smoothie_list')
+
+    return render(request, 'smoothies/smoothie_confirm_delete.html', {'smoothie': smoothie})
