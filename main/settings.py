c3ee3065909bf7a91d0c4698ca9ff577fd5ac7c3
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-03 23:29:44 +0100

    Add login redirect and logout redirect ot settings

--- main/settings.py
+++ main/settings.py
@@ -131,4 +131,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
