65bac1839453e01c8738758a84d234fa6c0df790
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-03 22:22:39 +0100

    Fix signup view and set up signup

--- smoothies/views.py
+++ smoothies/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .models import Smoothie
 from .forms import SmoothieForm
-
 
 def signup(request):
     if request.method == 'POST':
@@ -17,53 +16,33 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'smoothies/signup.html', {'form': form})
 
-
 def smoothie_list(request):
-    smoothies = Smoothie.objects.all().order_by('-date_created')
+    smoothies = Smoothie.objects.all()
     return render(request, 'smoothies/smoothie_list.html', {'smoothies': smoothies})
-
 
 @login_required
 def smoothie_create(request):
-    if request.method == 'POST':
-        form = SmoothieForm(request.POST)
-        if form.is_valid():
-            smoothie = form.save(commit=False)
-            smoothie.author = request.user
-            smoothie.save()
-            return redirect('smoothie_list')
-    else:
-        form = SmoothieForm()
+    form = SmoothieForm(request.POST or None)
+    if form.is_valid():
+        smoothie = form.save(commit=False)
+        smoothie.author = request.user
+        smoothie.save()
+        return redirect('smoothie_list')
     return render(request, 'smoothies/smoothie_form.html', {'form': form})
-
 
 @login_required
 def smoothie_update(request, pk):
-    smoothie = get_object_or_404(Smoothie, pk=pk)
-
-    if smoothie.author != request.user:
+    smoothie = get_object_or_404(Smoothie, pk=pk, author=request.user)
+    form = SmoothieForm(request.POST or None, instance=smoothie)
+    if form.is_valid():
+        form.save()
         return redirect('smoothie_list')
-
-    if request.method == 'POST':
-        form = SmoothieForm(request.POST, instance=smoothie)
-        if form.is_valid():
-            form.save()
-            return redirect('smoothie_list')
-    else:
-        form = SmoothieForm(instance=smoothie)
-
     return render(request, 'smoothies/smoothie_form.html', {'form': form})
-
 
 @login_required
 def smoothie_delete(request, pk):
-    smoothie = get_object_or_404(Smoothie, pk=pk)
-
-    if smoothie.author != request.user:
-        return redirect('smoothie_list')
-
+    smoothie = get_object_or_404(Smoothie, pk=pk, author=request.user)
     if request.method == 'POST':
         smoothie.delete()
         return redirect('smoothie_list')
-
     return render(request, 'smoothies/smoothie_confirm_delete.html', {'smoothie': smoothie})
