eaea3acf5faf917bca2ac869fa2c4abe0d3863e7
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-04 03:16:26 +0100

    Update allowed hosts in settings

--- main/settings.py
+++ main/settings.py
@@ -26,8 +26,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Application definition
 
