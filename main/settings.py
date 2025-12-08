bd454523c6dde21ae57d07e994b5294f99fb0379
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-12-08 23:45:09 +0100

    Final steps for Heroku deployment

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1" "https://smoothieshare-b6617971ad25.herokuapp.com/"]
 
 # Application definition
 
